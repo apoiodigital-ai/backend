250e0bf2db7daacf4975a8564ef47a858df54953
PsSergio <pscosta2008@gmail.com>
2026-04-18 10:38:34 -0300

    feat: adding new IA agents to system (A1, A4, A5). Improving documentation in GeminiService. .

--- apoiodigital/src/main/java/br/com/tucunare/apoiodigital/service/GeminiService.java
+++ apoiodigital/src/main/java/br/com/tucunare/apoiodigital/service/GeminiService.java
@@ -1,9 +1,6 @@
 package br.com.tucunare.apoiodigital.service;
 
-import br.com.tucunare.apoiodigital.dto.AppSuportadoToGeminiDTO;
-import br.com.tucunare.apoiodigital.dto.FindBestAppResponseDTO;
-import br.com.tucunare.apoiodigital.dto.RequestInputToGeminiDTO;
-import br.com.tucunare.apoiodigital.dto.RequisicaoInputDTO;
+import br.com.tucunare.apoiodigital.dto.*;
 import br.com.tucunare.apoiodigital.model.AppSuportado;
 import br.com.tucunare.apoiodigital.repository.AppSuportadoRepository;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -45,7 +42,6 @@ public class GeminiService {
 
     private Client client;
 
-    private String basicRules;
 
     @PostConstruct
     public void init() {
@@ -53,8 +49,7 @@ public class GeminiService {
         this.client = Client.builder()
                 .apiKey(apiKey)
                 .build();
-        
-        basicRules = getRules("src/main/resources/rules/basic-rules.txt");
+
     }
 
     public String analyzeText(String input, GenerateContentConfig config) {
@@ -86,12 +81,14 @@ public class GeminiService {
                 return GenerateContentConfig.builder()
                         .responseMimeType("application/json") // This is the "magic" line
                 .systemInstruction(Content.fromParts(
-                        Part.fromText(basicRules + "\n" +additionalRules)
+                        Part.fromText(additionalRules)
                 )).temperature(temp)
                 .build();
     }
 
-    public String definirTituloAtalho(String prompt){
+    // Gera um título através do prompt_limpo de 3 a 6 palavras
+    // TODO: MELHORAR REGRA...
+    public String definirTituloAtalho(String prompt_limpo){ // A5
         String additionalRules = getRules("src/main/resources/rules/defineTitleAtalho-rules.txt");
 
         ObjectMapper objectMapper = new ObjectMapper();
@@ -99,7 +96,7 @@ public class GeminiService {
         GenerateContentConfig config = generateConfig(additionalRules, 0.1f);
 
         try {
-            String response = analyzeText(prompt, config);
+            String response = analyzeText(prompt_limpo, config);
 
             JsonNode jsonNode = objectMapper.readTree(response);
 
@@ -113,6 +110,64 @@ public class GeminiService {
         }
     }
 
+    // Simplifica o prompt vindo do usuário e retorna prompt_limpo.
+    // o A1 reduz o prompt do usuário em prompts mais objetivos e menos complexos para os próximos agentes
+    public String simplificarPrompt(String prompt){ // A1
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        String rules = getRules("src/main/resources/rules/simplifyPrompt-rule.txt");
+
+        GenerateContentConfig config = generateConfig(rules, 0.1f);
+
+        try {
+            String input = objectMapper.writeValueAsString(prompt);
+
+            String response = analyzeText(input, config);
+
+            JsonNode jsonNode = objectMapper.readTree(response);
+
+            String promptLimpo = jsonNode.get("prompt_limpo").asText();
+
+            System.out.println("\nPrompt Limpo: " + promptLimpo);
+
+            return promptLimpo;
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // O GenerateContextAppDTO exige prompt_limpo, nome_app_instalado e nome_app_banco
+    // prompt_limpo é dado pelo A1
+    // nome_app_instalado vai ser pego por RequestInputToGeminiDTO.lista_apps_instalados.get(i), tal que i é dado pelo A2
+    // nome_app_banco vai ser pego por RequestInputToGeminiDTO.lista_apps_banco.get(j), tal que j é dado pelo A3
+    public String definirContexto(GenerateContextAppDTO dto){ // A4
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        String rules = getRules("src/main/resources/rules/defineContext-rule.txt");
+
+        GenerateContentConfig config = generateConfig(rules, 0.1f);
+
+        try {
+            String input = objectMapper.writeValueAsString(dto);
+
+            String response = analyzeText(input, config);
+
+            JsonNode jsonNode = objectMapper.readTree(response);
+
+            String promptLimpo = jsonNode.get("contexto").asText();
+
+            System.out.println("\nPrompt Limpo: " + promptLimpo);
+
+            return promptLimpo;
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // TODO: REESTRUTURAR FUNCAO
+    // Tal função vai ser reestruturada para "juntar" todos os agentes. O RequestInputToGeminiDTO possui todas as informacoes iniciais necessárias.
+    // A ideia é reunir todos os agentes e definir o fluxo definido (A0 -> A1 -> A2 -> A3 -> A4 -> A5) aqui
+    // NOTA: CRIAR OUTRA FUNCAO PARA A2 E A3... acharMelhorApp agora se refere à ação como um toodo.
     public FindBestAppResponseDTO acharMelhorApp(RequestInputToGeminiDTO dto) {
         ObjectMapper objectMapper = new ObjectMapper();
 
