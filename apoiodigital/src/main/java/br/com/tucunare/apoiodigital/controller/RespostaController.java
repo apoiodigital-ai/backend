1f3dc7d58abe0e60951cac400ab10cb1a05fe858
daniel marinho <danielmarinhom@gmail.com>
2026-05-22 15:10:06 -0300

    fix: iaagenty fixed

--- apoiodigital/src/main/java/br/com/tucunare/apoiodigital/controller/RespostaController.java
+++ apoiodigital/src/main/java/br/com/tucunare/apoiodigital/controller/RespostaController.java
@@ -2,6 +2,8 @@ package br.com.tucunare.apoiodigital.controller;
 
 import br.com.tucunare.apoiodigital.dto.IARespostaRawDTO;
 import br.com.tucunare.apoiodigital.dto.IARespostaRequestDescryptDTO;
+import br.com.tucunare.apoiodigital.dto.tutorial.*;
+import br.com.tucunare.apoiodigital.service.FindBestAnswerService;
 import br.com.tucunare.apoiodigital.service.IAService;
 import br.com.tucunare.apoiodigital.service.RespostaService;
 
@@ -12,28 +14,35 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+
 @RestController
 @RequestMapping("/resposta")
 public class RespostaController {
 
     private final RespostaService respostaService;
     private final IAService iaService;
+    private final FindBestAnswerService findBestAnswerService;
+
 
     public RespostaController(
             RespostaService respostaService,
-            IAService iaService
+            IAService iaService,
+            FindBestAnswerService findBestAnswerService
     ) {
         this.respostaService = respostaService;
         this.iaService = iaService;
+        this.findBestAnswerService = findBestAnswerService;
     }
 
-    @PostMapping("/exigir")
-    public ResponseEntity<IARespostaRawDTO> exigirRespostaDaIA(
-            @RequestBody IARespostaRequestDescryptDTO request
+
+    @PostMapping("/achar-resposta")
+    public ResponseEntity<FindBestAnswerResponseDTO> acharMelhorResposta(
+            @RequestBody IAAgentXTutorialRequestDTO request
     ) {
-        IARespostaRawDTO resposta = iaService.acharMelhorResposta(request);
-        return ResponseEntity.ok(resposta);
+        FindBestAnswerResponseDTO response = findBestAnswerService.findBestAnswer(request);
+        return ResponseEntity.ok(response);
     }
+
 
     @GetMapping("/listar/{idReq}")
     public ResponseEntity<List<Map<String, String>>> carregarRespostas(
@@ -43,4 +52,24 @@ public class RespostaController {
                 respostaService.listarRespostaPorRequisicao(idReq)
         );
     }
+
+
+    @PostMapping("/validar/necessidade-informacoes")
+    public ResponseEntity<ChecksInformationNeedsResponseDTO> checksInformationNeeds(
+            @RequestBody ChecksInformationNeedsRequestDTO requestDTO
+    ) {
+        ChecksInformationNeedsResponseDTO response = findBestAnswerService.checksInformationNeeds(requestDTO);
+        return ResponseEntity.ok(response);
+    }
+
+
+    @PostMapping("/validar/resposta-necessidade")
+    public ResponseEntity<IAAgentYTutorialResponseDTO> checksQuestionReturns(
+            @RequestBody IAAgentYTutorialRequestDTO request
+    ) {
+        IAAgentYTutorialResponseDTO response = findBestAnswerService.checksQuestionReturns(request);
+        if (!response.satisfaz()) return ResponseEntity.badRequest().body(response);
+        return ResponseEntity.ok(response);
+    }
+
 }
--- apoiodigital/src/main/java/br/com/tucunare/apoiodigital/dto/tutorial/IAAgentYTutorialResponseDTO.java
+++ apoiodigital/src/main/java/br/com/tucunare/apoiodigital/dto/tutorial/IAAgentYTutorialResponseDTO.java
@@ -2,6 +2,5 @@ package br.com.tucunare.apoiodigital.dto.tutorial;
 
 import br.com.tucunare.apoiodigital.enums.TiposPendencia;
 
-public record IAAgentYTutorialResponseDTO(boolean interromper,
-                                          TiposPendencia tipo_pendencia, String descricao_duvida) {
+public record IAAgentYTutorialResponseDTO(String pergunta, boolean satisfaz) {
 }
